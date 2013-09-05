b5d8fb107af389c4e8a86e42141668d5a792bbbf
Sarath <sarath.hacks@gmail.com>
2013-09-05 12:44:48 +0530

    First file

---
+++ methods.validation.jquery.js
@@ -0,0 +1,13 @@
+//Phone number validation 
+// +91 0000 -8888-22
+jQuery.validator.addMethod('phone',function(phone){
+  var vp=/^[+\(\)\[\]]*([0-9-][ \(\)\[\]]*){6,15}$/;
+	return vp.test(phone);
+});
+ 
+ 
+//Human name {Charecters and spacees only}
+jQuery.validator.addMethod('human_name',function(name){
+	var v_hn=/^[A-Za-z \]{}[A-Za-z]{1,}$/;
+	return v_hn.test(name);
+});
